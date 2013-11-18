2b04cd3c0bfe08e8e859bdb30610f156aadc2b3d
Daisuke Oka <daisuke_oka@nanosoftware.biz>
2013-11-19 08:35:30 +0900

    インターフェースを実装しました

--- BrainStorming/BrainStormingFirstViewController.h
+++ BrainStorming/BrainStormingFirstViewController.h
@@ -6,8 +6,34 @@
 //  Copyright (c) 2013年 岡 大輔. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 
-@interface BrainStormingFirstViewController : UIViewController
+//ユーザーインクルード
+
+#import "BrainStormingAppDelegate.h"
+#import "WordStore.h"
+#import "WordGenerate.h"
+#import "GeneralWordStore.h"
+#import "ITWordStore.h"
+#import "ChemicalWordStore.h"
+#import "WordSetPlottingScrollView.h"
+#import "OpticalWordStore.h"
+
+
+#import "iAd/iAd.h"
+
+
+
+
+
+@interface BrainStormingFirstViewController : UIViewController 
+
+
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event;
+
+@property (weak, nonatomic) IBOutlet WordSetPlottingScrollView *scroll;
+
+
+
 
 @end
