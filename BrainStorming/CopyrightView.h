00c65ca6a5b1dacd9e753d091fbc71f7bdd66c78
Daisuke Oka <daisuke_oka@nanosoftware.biz>
2013-11-19 08:38:16 +0900

    実装しました

---
+++ BrainStorming/CopyrightView.h
@@ -0,0 +1,15 @@
+//
+//  CopyrightView.h
+//  BrainStorming
+//
+//  Created by 岡 大輔 on 2013/10/05.
+//  Copyright (c) 2013年 岡 大輔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CopyrightView : UITextView
+
+-(void)InputCopyrightInformation;
+
+@end
