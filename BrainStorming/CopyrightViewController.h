b11574ff1bbb82052b5c4d05104be7be16adc160
Daisuke Oka <daisuke_oka@nanosoftware.biz>
2013-11-19 08:38:52 +0900

    ファイルを加えました

---
+++ BrainStorming/CopyrightViewController.h
@@ -0,0 +1,20 @@
+//
+//  CopyrightViewController.h
+//  BrainStorming
+//
+//  Created by 岡 大輔 on 2013/10/05.
+//  Copyright (c) 2013年 岡 大輔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//ユーザーインクルード
+#import "CopyrightView.h"
+
+
+@interface CopyrightViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet  CopyrightView *copyright1;
+
+
+@end
