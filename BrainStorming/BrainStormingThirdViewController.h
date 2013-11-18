c4490afe65eaec3c01fc9d8ad19dfc39bc0b9576
Daisuke Oka <daisuke_oka@nanosoftware.biz>
2013-11-19 08:36:27 +0900

    実装しました

--- BrainStorming/BrainStormingThirdViewController.h
+++ BrainStorming/BrainStormingThirdViewController.h
@@ -8,6 +8,40 @@
 
 #import <UIKit/UIKit.h>
 
-@interface BrainStormingThirdViewController : UIViewController
+
+
+//ユーザーインクルード
+#import "BrainStormingFirstViewController.h"
+#import "BrainStormingAppDelegate.h"
+#import "UserDefaultDataForBrainstormingSecondView.h"
+
+#import "InnerDatabaseSearch.h"
+
+@interface BrainStormingThirdViewController : UIViewController <UISearchBarDelegate>
+
+
+//@property (weak, nonatomic) IBOutlet UIScrollView *WordScrollView;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *WordNumberSegmentedControl;
+
+@property (weak, nonatomic) IBOutlet UISwitch *GeneralWordSwitch;
+
+@property (weak, nonatomic) IBOutlet UISwitch *ITWordSwitch;
+
+@property (weak, nonatomic) IBOutlet UISwitch *ChemicalWordSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *PhysicalWordSwitch;
+
+@property (weak, nonatomic) IBOutlet UISwitch *ModernSocialWordSwitch;
+
+@property (weak, nonatomic) IBOutlet UISwitch *BiologicalWordSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *OpticalWordSwitch;
+
+
+@property (weak, nonatomic) IBOutlet UISearchBar *search;
+
+@property (weak, nonatomic) IBOutlet UILabel *WordCountLabel;
+@property (weak, nonatomic) IBOutlet UIButton *copyrightinformationbutton;
+
+-(void)initializeWordNumSegmentedControl;
+-(void)initializeUI;
 
 @end
