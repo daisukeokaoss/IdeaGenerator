16f6df003012f4daffcbca9882954b76936251b3
Daisuke Oka <daisuke_oka@nanosoftware.biz>
2013-11-19 08:33:45 +0900

    インポートしました

--- BrainStorming/WordHistoryRecorder.m
+++ BrainStorming/WordHistoryRecorder.m
@@ -8,6 +8,50 @@
 
 #import "WordHistoryRecorder.h"
 
+#define WORDARRAY_KEY @"WordArray_Key"
+#define WORDARRAY_COUNT_KEY @"WordArrayCount"
+
+
 @implementation WordHistoryRecorder
+
+-(void)InitializeSetting
+{
+    NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
+    self.MaximumWordCount = [ud integerForKey:WORDARRAY_COUNT_KEY];
+    
+    self.WordArray = [ud objectForKey: WORDARRAY_KEY];
+}
+
+-(void)FinalizeSetting
+{
+    NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
+    [ud setInteger:self.MaximumWordCount forKey:WORDARRAY_COUNT_KEY];
+    [ud setObject:self.WordArray forKey:WORDARRAY_KEY];
+    [ud synchronize];
+}
+
+-(void)PushToWordHistory:(NSString *)word
+{
+    if([self.WordArray count] >= self.MaximumWordCount){
+        for(int i=0;i<[self.WordArray count]-1;i++){
+            self.WordArray[i]= self.WordArray[i+1];
+        }
+        [self.WordArray addObject:word];
+    }else{
+        [self.WordArray addObject:word];
+    }
+    
+}
+
+-(NSString *)ReturnHistoryWord:(int)index
+{
+    if(index>=self.MaximumWordCount){
+        return nil;
+    }else if(index < 0){
+        return nil;
+    }else{
+        return self.WordArray[index];
+    }
+}
 
 @end
