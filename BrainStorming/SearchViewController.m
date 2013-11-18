415cbbfea5e50986302d3813359892737de791c8
Daisuke Oka <daisuke_oka@nanosoftware.biz>
2013-11-19 08:36:46 +0900

    実装しました

--- BrainStorming/SearchViewController.m
+++ BrainStorming/SearchViewController.m
@@ -10,6 +10,12 @@
 
 @interface SearchViewController ()
 
+{
+@private
+    UIWebView* webView;
+    UIButton *btn;
+}
+
 @end
 
 @implementation SearchViewController
@@ -27,7 +33,49 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    
+    //////////////////////////////////////////////////////////
+    //ユーザーエージェントを設定
+    NSString *modelname = [ [ UIDevice currentDevice] model];
+    if([modelname isEqualToString:@"iPad Simulator"]||[modelname isEqualToString:@"iPad"]){
+        
+            NSDictionary *dictionnary = [[NSDictionary alloc] initWithObjectsAndKeys:@"Mozilla/5.0 (iPad; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53", @"UserAgent", nil];
+            [[NSUserDefaults standardUserDefaults] registerDefaults:dictionnary];
+    }
+    webView = [[UIWebView alloc] initWithFrame: CGRectMake(self.view.bounds.origin.x, self.view.bounds.origin.y +80, self.view.bounds.size.width,self.view.bounds.size.height-80)];
+    //webView.frame = self.view.bounds;
+    webView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    webView.scalesPageToFit = YES;
+    [self.view addSubview:webView];
+   // NSURLRequest* request = [NSString stringWithFormat:@"http://www.google.co.jp/search?q=&#37;@", "XCode"];
+
+    
+    
+    NSString *query  = (__bridge NSString *)CFURLCreateStringByAddingPercentEscapes(NULL, (CFStringRef)self.SearchString, NULL, CFSTR (";,/?:@&=+$#"), kCFStringEncodingUTF8);
+    
+    
+    NSString *url = [NSString stringWithFormat:@"http://www.google.co.jp/m/search?q=%@",query];
+    NSURLRequest* request = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
+    [webView loadRequest:request];
+    
+    btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    btn.frame = CGRectMake(0, 20, self.view.bounds.size.width, 60);
+    [btn setBackgroundColor:[UIColor whiteColor]];
+
+    [btn setTitle:@"戻る" forState:UIControlStateNormal];
+    [btn setTitle:@"戻る" forState:UIControlStateHighlighted];
+    [btn setTitle:@"戻る" forState:UIControlStateDisabled];
+    // ボタンがタッチダウンされた時にhogeメソッドを呼び出す
+    [btn addTarget:self action:@selector(hoge:)
+  forControlEvents:UIControlEventTouchDown];
+    [self.view addSubview:btn];
 }
+
+-(void)hoge:(UIButton*)button{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
 
 - (void)didReceiveMemoryWarning
 {
