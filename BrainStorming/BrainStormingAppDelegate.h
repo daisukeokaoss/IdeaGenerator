1846862a8cb3804084ab30c8cba76a82755c9a60
Daisuke Oka <daisuke_oka@nanosoftware.biz>
2013-11-19 08:41:04 +0900

    じっそうしました

--- BrainStorming/BrainStormingAppDelegate.h
+++ BrainStorming/BrainStormingAppDelegate.h
@@ -6,10 +6,19 @@
 //  Copyright (c) 2013年 岡 大輔. All rights reserved.
 //
 
+
+
 #import <UIKit/UIKit.h>
+
+//ユーザーインクルード
+#import "ControlUI.h"
+
 
 @interface BrainStormingAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+
+
+@property (strong,nonatomic) ControlUI *controlUI;
 
 @end
