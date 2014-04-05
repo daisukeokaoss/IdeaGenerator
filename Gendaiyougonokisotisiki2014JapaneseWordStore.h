c5faa4721c426c24b1a4713cffb584a8298a13f8
Daisuke Oka <daisuke_oka@nanosoftware.biz>
2014-04-05 18:55:45 +0900

    現代用語追加

---
+++ Gendaiyougonokisotisiki2014JapaneseWordStore.h
@@ -0,0 +1,15 @@
+//
+//  Gendaiyougonokisotisiki2014JapaneseWordStore.h
+//  BrainStorming
+//
+//  Created by 岡 大輔 on 2014/04/05.
+//  Copyright (c) 2014年 岡 大輔. All rights reserved.
+//
+
+#import "WordStore.h"
+
+@interface Gendaiyougonokisotisiki2014JapaneseWordStore : WordStore
+
+-(NSMutableArray *)GenerateWordArray;
+
+@end
