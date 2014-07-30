62b44c76d0bca7c65d99691064e0a9fbb561baf1
Daisuke Oka <daisuke_oka@nanosoftware.biz>
2014-07-30 14:11:57 +0900

    軍事用語追加

---
+++ BrainStorming/JapaneseMilitaryWordStore.h
@@ -0,0 +1,15 @@
+//
+//  JapaneseMilitaryWordStore.h
+//  BrainStorming
+//
+//  Created by おかやん on 2014/07/30.
+//  Copyright (c) 2014年 岡 大輔. All rights reserved.
+//
+
+#import "WordStore.h"
+
+@interface JapaneseMilitaryWordStore : WordStore
+
+-(NSMutableArray *)GenerateWordArray;
+
+@end
