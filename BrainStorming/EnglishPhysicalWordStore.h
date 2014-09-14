d9f2199ec7698793c1ef958aadcfbacf9f79d9a3
おかやん <okadaisuke@okayan-no-MacBook-Pro.local>
2014-09-14 11:12:19 +0900

    物理英語用語追加

---
+++ BrainStorming/EnglishPhysicalWordStore.h
@@ -0,0 +1,13 @@
+//
+//  EnglishPhysicalWordStore.h
+//  BrainStorming
+//
+//  Created by おかやん on 2014/09/14.
+//  Copyright (c) 2014年 岡 大輔. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EnglishPhysicalWordStore : NSObject
+
+@end
