2db5e1ab6897e0c526eae567a66f1b7ad666e3a4
Daisuke Oka <daisuke_oka@nanosoftware.biz>
2014-09-17 11:16:44 +0900

    医療単語　機械追加

---
+++ BrainStorming/MedicalWordJp_MachineCollect.h
@@ -0,0 +1,15 @@
+//
+//  MedicalWordJp_MachineCollect.h
+//  BrainStorming
+//
+//  Created by おかやん on 2014/09/17.
+//  Copyright (c) 2014年 岡 大輔. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MedicalWordJp_MachineCollect : NSObject
+
++(NSMutableArray *)GenerateWordArray;
+
+@end
