cf79a15888d736b0ba96eaeff4a634dfa8186ba0
Daisuke Oka <daisuke_oka@nanosoftware.biz>
2014-09-23 00:13:19 +0900

    コンピュータ用語追加

---
+++ ComputerWord_machineCollect.h
@@ -0,0 +1,15 @@
+//
+//  ComputerWord_machineCollect.h
+//  BrainStorming
+//
+//  Created by おかやん on 2014/09/22.
+//  Copyright (c) 2014年 岡 大輔. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ComputerWord_machineCollect : NSObject
+
++(NSMutableArray *)GenerateWordArray;
+
+@end
