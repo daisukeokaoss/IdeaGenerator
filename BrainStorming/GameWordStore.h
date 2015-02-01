f1df3396409d827293cc7e8ff86cc865dcc06c10
Daisuke Oka <daisuke_oka@nanosoftware.biz>
2015-02-01 21:28:03 +0900

    ゲーム用語追加

---
+++ BrainStorming/GameWordStore.h
@@ -0,0 +1,15 @@
+//
+//  GameWordStore.h
+//  BrainStorming
+//
+//  Created by おかやん on 2015/02/01.
+//  Copyright (c) 2015年 岡 大輔. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameWordStore : NSObject
+
++(NSMutableArray *)GenerateWordArray;
+
+@end
